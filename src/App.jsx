0094af47253c0d082f46548a13ba59afd30a68d1
Lucas Anselmo <30813248+lucas62@users.noreply.github.com>
2022-08-20 15:23:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Sidebar } from './components/Sidebar'
+import { Header } from './components/Header'
+import { Post } from './Post'
+
+import styles from './App.module.css'
+import './global.css'
+
+/**
+ * The App function returns the Header component, the Sidebar component, and the
+ * Post component
+ */
+function App() {
+  return (
+    <>
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post />
+          <Post />
+        </main>
+
+      </div>
+    </>
+  )
+}
+
+export default App
