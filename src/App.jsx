bb4b708c96945f61e5f6bdb09dea77e7ee7f59b9
Lucas Anselmo <30813248+lucas62@users.noreply.github.com>
2022-08-25 14:46:46 -0300

    Mok data to posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,45 @@ import { Post } from './components/Post'
 import styles from './App.module.css'
 import './global.css'
 
-
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:'http://github.com/lucas62.png',
+      name: 'Lucas Anselmo',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    tags: [
+      '#novoprojeto',
+      '#nlw',
+      '#rocketseat'
+    ],
+    publishedAt: new Date('2022-08-24 18:30:50')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:'http://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala pessoal 👋'},
+      {type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻 Acesse e deixe seu feedback 👉'},
+      {type: 'link', content: 'devonlane.design'}
+    ],
+    tags: [
+      '#uiux',
+      '#userexperience'
+    ],
+    publishedAt: new Date('2022-08-25 10:30:30')
+  },
+]
 
 /**
  * The App function returns the Header component, the Sidebar component, and the
@@ -20,7 +58,15 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post />
+          {posts.map((post, index) => (
+            <Post
+              key={index} 
+              author={post.author}
+              content={post.content}
+              tags={post.tags}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
 
       </div>
